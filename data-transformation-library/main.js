cd987e75f4f96391a1cd552f779f178b232badc7
Federico Acosta <fede.acosta2396@gmail.com>
2025-06-10 20:01:55 -0300

    created data transformation library

---
+++ data-transformation-library/main.js
@@ -0,0 +1,17 @@
+export const DataTransform = {
+  addValues: (a, b) => {
+    if (typeof a === number && typeof b === number) return a + b;
+    if (typeof a === "string" || typeof b === "string")
+      return String(a) + String(b);
+    throw new Error("Incompatible Types");
+  },
+  stringifyValue: (value) => {
+    if (typeof value === "object" && value !== null)
+      return JSON.stringify(value);
+    return String(value);
+  },
+  invertBoolean: (value) => {
+    if (typeof value !== "boolean") throw new Error("Incorrect Type");
+    return !value;
+  },
+};
